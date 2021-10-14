b778b5c17ab4d9e413b48b5b8e8eee8ccb7103c3
Jared Davidson (Archetapp) <archetapp@gmail.com>
2021-10-14 17:39:31 -0600

    Initial Commit

---
+++ Shared/UnderlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnderlineApp.swift
+//  Shared
+//
+//  Created by Jared Davidson on 10/14/21.
+//
+
+import SwiftUI
+
+@main
+struct UnderlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
